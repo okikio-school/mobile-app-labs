31c978b36fc832943adb06476737bed0d12de7f9
Johvonne <110308362+Johvonnegk@users.noreply.github.com>
2024-09-24 13:14:56 -0400

    name changes and buttons added
    color buttons added to notes section and variable names changed

--- Lab1/NoteMy/app/src/main/java/com/example/notemy/NewNotesScreen.kt
+++ Lab1/NoteMy/app/src/main/java/com/example/notemy/NewNotesScreen.kt
@@ -1,7 +1,9 @@
 package com.example.notemy
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,6 +11,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class NewNotesScreen : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -19,17 +22,24 @@ class NewNotesScreen : AppCompatActivity() {
             insets
         }
 
-        val button1 = findViewById<Button>(R.id.button);
+        val button1 = findViewById<Button>(R.id.back_btn);
         button1.setOnClickListener {
             // Call the launch method to switch to SecondActivity
             launch(MainActivity::class.java)
         }
 
 
-        val button2 = findViewById<Button>(R.id.button2);
+        val button2 = findViewById<Button>(R.id.save_btn);
         button2.setOnClickListener {
             // Call the launch method to switch to SecondActivity
             launch(MainActivity::class.java)
+        }
+        val blueBtn = findViewById<Button>(R.id.color_btn_blue)
+        val deepGreenBtn = findViewById<Button>(R.id.color_btn_deep_green)
+        val pastelPink = findViewById<Button>(R.id.color_btn_pastel_pink)
+        val noteBackground = findViewById<View>(R.id.background)
+        blueBtn.setOnClickListener{
+            noteBackground.setBackgroundColor(R.color.blue)
         }
     }
 
